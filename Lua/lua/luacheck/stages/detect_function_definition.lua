e194d5535453d72e87d3207fc5a8bba312e4ad41
jinshenzhe <jinshenzhe@outlook.com>
2021-04-22 11:53:48 +0800

    增加函数名过滤名单

--- Lua/lua/luacheck/stages/detect_function_definition.lua
+++ Lua/lua/luacheck/stages/detect_function_definition.lua
@@ -4,7 +4,7 @@ local ipairs = ipairs
 
 local sfind = string.find
 local sbyte = string.byte
-local sub = string.sub
+local ssub = string.sub
 local slen = string.len
 
 stage.warnings = {
@@ -12,6 +12,11 @@ stage.warnings = {
       fields = {"function_name"}},
    ["911"] = {message_format = "'arg' is not allowed as a parameter name",
       fields = {}},
+}
+
+local function_name_whitelist = {
+   "ctor",
+   "new"
 }
 
 local function is_closure_function(node_parent)
@@ -23,22 +28,34 @@ local function is_closure_function(node_parent)
    return true
 end
 
+local function is_in_whitelist(function_name)
+   for _, name in ipairs(function_name_whitelist) do
+      if function_name == name then
+         return true
+      end
+   end
+   return false
+end
+
 local function warn_function_name(chstate, node)
    local temp_table = node[1]
    if temp_table then
       if is_closure_function(temp_table[1]) == false then
-         local name = node.name
-         if name then
-            local _, index = sfind(name, "%.", 1)
+         local full_name = node.name
+         if full_name then
+            local _, index = sfind(full_name, "%.", 1)
             if not index then
                index = 0
             end
-            local num_byte = sbyte(name, index + 1)
-            local num_len = slen(name) - index
-            if num_byte < 65 or num_byte > 91 or num_len < 2 then
-               chstate:warn_range("811", node, {
-                  function_name = node.name
-               })
+            local name = ssub(full_name, index + 1)
+            if is_in_whitelist(name) == false then
+               local num_byte = sbyte(name, 1)
+               local num_len = slen(name)
+               if num_byte < 65 or num_byte > 91 or num_len <= 1 then
+                  chstate:warn_range("811", node, {
+                     function_name = node.name
+                  })
+               end
             end
          end
       end
